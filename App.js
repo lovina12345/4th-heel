c0940891c87c008e10485fc31cda8d63754a4869
lovina <lovinaafrifa223344@gmail.com>
2022-05-17 08:06:13 -0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Text,View,ScrollView,SafeAreaView,FlatList,Image} from "react-native";
+// import { FontAwesome, MaterialCommunityIcons, Ionicons,} from "@expo/vector-icons";
+import React from "react";
 
-export default function App() {
+
+const App = () => {
+  const students = [
+     {
+       name: "Sandals Heels",
+       email: "$50",
+     profile: require("./assets/6.jpg"),
+     },
+    
+
+    {
+      name: "Block heels",
+      email: "70$",
+      profile: require("./assets/7.jpg"),
+    },
+
+    {
+      name: "slippers heels",
+      email: "$60",
+      profile: require("./assets/8.jpg"),
+    },
+
+    {
+      name: "Block heel",
+      email: "$80",
+      profile: require("./assets/9.jpg"),
+    },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <FlatList
+        data={students}
+        keyExtractor={(item, index) => item.name + index}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              marginVertical: 4,
+              padding: 16,
+              marginLeft: 20,
+            }}
+          >
+            <View
+              style={{
+                flex: 20,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Image
+                source={item.profile}
+                style={{ height: 140, width: 100, borderRadius: 15 }}
+              />
+            </View>
+            <View
+              style={{
+                flex: 40,
+                justifyContent: "space-evenly",
+                paddingHorizontal: 16,
+              }}
+            >
+              <Text style={{ fontSize: 20, fontWeight: "bold", paddingLeft: 20,}}>
+                {item.name}
+              </Text>
+              <Text style={{ fontSize: 16 , paddingLeft: 40,}}>{item.email}</Text>
+            </View>
+            <View
+              style={{
+                flex: 40,
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-around",
+              }}
+            >
+              {/* <FontAwesome name="phone" size={24} color="#45bcff" />
+              <MaterialCommunityIcons
+                name="message-processing"
+                size={24}
+                color="#45bcff"
+              />
+              <Ionicons name="ios-ellipsis-vertical" size={24} color="black" /> */}
+            </View> 
+          </View>
+        )}
+      />  
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
+
+
